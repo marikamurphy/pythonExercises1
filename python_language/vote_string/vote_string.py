a28b9567254dabd00ab2985f10aa8b10facc6a71
Marika Murphy <marikamurphy18@gmail.com>
2018-02-02 16:31:55 -0600

    Attempting to add vote_string

---
+++ python_language/vote_string/vote_string.py
@@ -0,0 +1,17 @@
+"""
+Vote String
+-----------
+You have the string below, which is a set of "yes/no" votes,
+where "y" or "Y" means yes and "n" or "N" means no. Determine 
+the percentages of yes and no votes.
+
+::
+
+    votes = "y y n N Y Y n n N y Y n Y"
+
+See :ref:`vote-string-solution`.
+"""
+# Begin exercise
+
+votes = "y y n N Y Y n n N y Y n Y"
+
