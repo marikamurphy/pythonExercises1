297520a0fd6ef2432e395bc16cac5115c3eb2aba
Marika Murphy <marikamurphy18@gmail.com>
2018-02-06 09:19:05 -0600

    Finished mpl_plotting.py

--- matplotlib/mpl_plotting/mpl_plotting.py
+++ matplotlib/mpl_plotting/mpl_plotting.py
@@ -55,26 +55,33 @@ plt.subplot(2,3,1)
 plt.plot(y1_noisy,'k-^')
 # 4. Overwrite the top left subplot with a plot of y1_noisy using a line plot
 #    with a red dashed line.
-
+plt.cla()
 plt.plot(y1_noisy,'r--')
 # 5. In the top, middle subplot, plot y2_noisy using a line plot with
 #    green circle markers.
-
+plt.subplot(2,3,2)
+plt.plot(y2_noisy,'go')
 # 6. In the top, right subplot, plot y1_noisy vs. y2_noisy using a scatter plot
 #    with blue circles.
-
+plt.subplot(2,3,3)
+plt.scatter(y1_noisy,y2_noisy,c='b')
 # 7. In the bottom, left subplot, plot the histogram of (y2_noisy - y1_noisy)
-
+plt.subplot(2,3,4)
+plt.hist(y2_noisy-y1_noisy)
 # 8. In the bottom, middle subplot, plot the histograms side by side for
 #    y1_noisy and y2_noisy (set hold to True again first).
-
+plt.subplot(2,3,5)
+plt.hist([y1_noisy,y2_noisy])
 # 9. In the bottom, right subplot, plot the correlation matrix of
 #        x1, y1, e1, y1_noisy, x2, y2, e2, y2_noisy
 #    as an image.
 #    Hint: syntax for correlation matrix is
 #        result = np.corrcoef([arr1, arr2, arr3, ...])
-
+plt.subplot(2,3,6)
+matrix = np.corrcoef([x1,y1,e1,y1_noisy,x2,y2,e2,y2_noisy])
+plt.imshow(matrix,interpolation='none')
 # 10. Add a colorbar to the subplot.
+plt.colorbar()
 
 # Make sure that the figure window is updated when executing this file
 # as a script.
